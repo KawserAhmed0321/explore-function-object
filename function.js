421f4cb5bcc82a75fa5483a0e4f81c1009721d25
KawserAhmed0321 <kanonmmc@gmail.com>
2022-02-01 09:32:55 +0600

    function

---
+++ function.js
@@ -0,0 +1,15 @@
+// function declaration 
+function turnOn() {
+    console.log('walk towards the sawitch');
+    console.log('press the switch');
+}
+// call function 
+turnOn();
+
+
+// another function 
+function nameList() {
+    var namelist = ['anita', 'anika', 'anisa'];
+    console.log(namelist);
+}
+nameList();
