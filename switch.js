be35dcd5af516fbb9b1cf9b2c4e021b995c82773
KawserAhmed0321 <kanonmmc@gmail.com>
2022-02-01 10:35:15 +0600

    add switch

---
+++ switch.js
@@ -0,0 +1,13 @@
+switch ('') {
+    case 'red':
+        console.log('color is red');
+        break;
+    case 'blue':
+        console.log('color is blue');
+        break;
+    case 'white':
+        console.log('color is white');
+        break;
+    default:
+        console.log('color is black');
+};
