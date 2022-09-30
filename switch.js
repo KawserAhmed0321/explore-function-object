fc0cee33c11000ec7882cb34340f38e53efe1fa7
KawserAhmed0321 <kanonmmc@gmail.com>
2022-10-01 03:12:48 +0600

    switch

---
+++ switch.js
@@ -0,0 +1,36 @@
+var color ='sky';
+if(color == 'white')
+{
+    console.log('color is white');
+}
+else if( color == 'red')
+{
+    console.log('color is red');
+}
+else if(color == 'green')
+{
+    console.log('color is green');
+}
+else{
+    console.log('color is black');
+}
+
+
+//switch case
+switch(color)
+{
+    case 'green':
+        console.log('color is green');
+        break;
+    
+  case 'red' :
+        console.log('color is red');
+        break;
+
+    case 'white' :
+        console.log('color is white');
+        break;
+    
+    default :
+        console.log('color is sky');
+}
