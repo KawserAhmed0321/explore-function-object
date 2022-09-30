13320145c6f795d4dfb195063636728821e3daba
KawserAhmed0321 <kanonmmc@gmail.com>
2022-10-01 03:23:02 +0600

    conveter

---
+++ convator.js
@@ -0,0 +1,14 @@
+function inchToFeet(inches) {
+    var feet = inches / 12;
+    return feet;
+
+}
+var myInches = 144;
+var feet = inchToFeet(myInches);
+console.log('my feet:',feet);
+
+
+
+var anitaInches = 142;
+var feet = inchToFeet(anitaInches);
+console.log('anita feet:', feet);
