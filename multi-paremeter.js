64c6dd440981618f415265d077aa5a19f67d54f6
KawserAhmed0321 <kanonmmc@gmail.com>
2022-10-01 02:24:13 +0600

    multi-paremeter

---
+++ multi-paremeter.js
@@ -0,0 +1,11 @@
+function add(num1,num2)
+{
+    console.log(num1,num2);
+    var  total = num1 + num2;
+    return total;
+
+}
+var firstnumber=21;
+var secondnumber =22;
+var Total  =add(firstnumber,secondnumber);
+console.log(Total);
